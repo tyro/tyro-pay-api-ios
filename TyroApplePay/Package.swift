0cc36a7a0c788544ec693d7eca294b24a55b87b0
Ronaldo Gomes <rgomes@tyro.com>
2024-01-24 12:07:54 +1100

    testing github actions

--- TyroApplePay/Package.swift
+++ TyroApplePay/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "TyroApplePay",
-  platforms: [.macOS(.v14), .iOS(.v13)],
+  platforms: [.macOS(.v11), .iOS(.v13)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
@@ -13,7 +13,8 @@ let package = Package(
       targets: ["TyroApplePay"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Nimble.git", from: "13.2.0")
+    .package(url: "https://github.com/Quick/Nimble.git", from: "13.2.0"),
+    .package(url: "https://github.com/Quick/Quick.git", from: "7.4.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,7 +23,7 @@ let package = Package(
       name: "TyroApplePay"),
     .testTarget(
       name: "TyroApplePayPackageTests",
-      dependencies: ["TyroApplePay", "Nimble"]),
+      dependencies: ["TyroApplePay", "Quick", "Nimble"]),
   ]
   // swiftLanguageVersions: [.version("5.7.1")]
 )
---
+++ TyroApplePay/Tests/TyroApplePayPackageTests/TyroApplePayPackageTests.swift
@@ -0,0 +1,15 @@
+import Quick
+import Nimble
+@testable import TyroApplePay
+
+final class TyroApplePayPackageTests: QuickSpec  {
+  override class func spec() {
+
+    describe("Sample test") {
+      it("should be true") {
+        expect(true).to(beTruthy())
+      }
+    }
+
+  }
+}
