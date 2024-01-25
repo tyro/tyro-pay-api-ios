edbc30d46c0a719ba829c3bc428b3771b1747889
Ronaldo Gomes <rgomes@tyro.com>
2024-01-25 11:36:01 +1100

    WIP

--- TyroApplePay/Sources/TyroApplePay/TyroApplePay.swift
+++ TyroApplePay/Sources/TyroApplePay/TyroApplePay.swift
@@ -24,57 +24,57 @@ public class TyroApplePay: NSObject {
             PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: supportedNetworks))
   }
   
-//  public func startPayment(completion: @escaping PaymentCompletionHandler) {
-//    
-//    completionHandler = completion
-//    
-//    let ticket = PKPaymentSummaryItem(label: "Festival Ticket", amount: NSDecimalNumber(string: "9.99"), type: .final)
-//    let tax = PKPaymentSummaryItem(label: "Tax", amount: NSDecimalNumber(string: "1.00"), type: .final)
-//    let total = PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "10.99"), type: .final)
-//    var paymentSummaryItems = [PKPaymentSummaryItem]()
-//    paymentSummaryItems = [ticket, tax, total]
-//    
-//    let paymentRequest = PKPaymentRequest()
-//    paymentRequest.paymentSummaryItems = paymentSummaryItems
-//    paymentRequest.merchantIdentifier = Configuration.Merchant.identifier
-//    paymentRequest.merchantCapabilities = [.credit]
-//    paymentRequest.countryCode = "AU"
-//    paymentRequest.currencyCode = "AUD"
-//    paymentRequest.supportedNetworks = TyroApplePay.supportedNetworks
-//    //    paymentRequest.shippingType = .delivery
-//    //    paymentRequest.shippingMethods
-//    //    paymentRequest.require
-//    
-//    let paymentController: PKPaymentAuthorizationController? = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
-//    paymentController?.delegate = self
-//    paymentController?.present(completion: { (presented: Bool) in
-//      if presented {
-//        print("Presented payment controller")
-//      } else {
-//        print("Failed to present payment controller")
-//      }
-//    })
-//
-//  }
+  public func startPayment(completion: @escaping PaymentCompletionHandler) {
+    
+    completionHandler = completion
+    
+    let ticket = PKPaymentSummaryItem(label: "Festival Ticket", amount: NSDecimalNumber(string: "9.99"), type: .final)
+    let tax = PKPaymentSummaryItem(label: "Tax", amount: NSDecimalNumber(string: "1.00"), type: .final)
+    let total = PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "10.99"), type: .final)
+    var paymentSummaryItems = [PKPaymentSummaryItem]()
+    paymentSummaryItems = [ticket, tax, total]
+    
+    let paymentRequest = PKPaymentRequest()
+    paymentRequest.paymentSummaryItems = paymentSummaryItems
+    paymentRequest.merchantIdentifier = Configuration.Merchant.identifier
+    paymentRequest.merchantCapabilities = [.credit]
+    paymentRequest.countryCode = "AU"
+    paymentRequest.currencyCode = "AUD"
+    paymentRequest.supportedNetworks = TyroApplePay.supportedNetworks
+    //    paymentRequest.shippingType = .delivery
+    //    paymentRequest.shippingMethods
+    //    paymentRequest.require
+    
+    let paymentController: PKPaymentAuthorizationController? = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
+    paymentController?.delegate = self
+    paymentController?.present(completion: { (presented: Bool) in
+      if presented {
+        print("Presented payment controller")
+      } else {
+        print("Failed to present payment controller")
+      }
+    })
+
+  }
   
 }
 
-//extension TyroApplePay: PKPaymentAuthorizationControllerDelegate {
-//  
-//  public func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
-//    completion(PKPaymentAuthorizationStatus.success)
-//  }
-//  
-//  public func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
-//    controller.dismiss {
-//      DispatchQueue.main.async {
-//        self.completionHandler!(true)
-//      }
-//    }
-//  }
-//  
-//  public func presentationWindow(for controller: PKPaymentAuthorizationController) -> UIWindow? {
-//    return nil
-//  }
-//  
-//}
+extension TyroApplePay: PKPaymentAuthorizationControllerDelegate {
+  
+  public func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
+    completion(PKPaymentAuthorizationStatus.success)
+  }
+  
+  public func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
+    controller.dismiss {
+      DispatchQueue.main.async {
+        self.completionHandler!(true)
+      }
+    }
+  }
+  
+  public func presentationWindow(for controller: PKPaymentAuthorizationController) -> UIWindow? {
+    return nil
+  }
+  
+}
