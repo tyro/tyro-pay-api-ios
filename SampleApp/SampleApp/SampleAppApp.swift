ab13dc13bc2cbdaebcc5bdc88d04dee8edd3950b
Christopher Grantham <cgrantham.tyro@gmail.com>
2024-01-25 16:47:58 +1100

    SampleApp initial commit.

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Christopher Grantham on 25/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
