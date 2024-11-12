9da2ff253fc6089a2798092a3c3351351a57abc5
Ronaldo Gomes <rongomes15@gmail.com>
2024-11-12 12:37:01 +1100

    update factory library

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     .package(url: "https://github.com/realm/SwiftLint.git", from: "0.54.0"),
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.53.1"),
     .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "2.0.0")),
-    .package(url: "https://github.com/hmlongco/Factory.git", from: "2.3.1")
+    .package(url: "https://github.com/hmlongco/Factory.git", from: "2.4.1")
   ],
   targets: [
     .target(
